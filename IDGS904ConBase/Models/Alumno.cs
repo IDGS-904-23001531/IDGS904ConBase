abb1450e153411654bb0e26b6997ec72f8b4d36e
Stefania Pro <stefaniapro5@gmail.com>
2026-06-11 23:23:09 -0600

    CRUD Alumnos 11/06/2026

---
+++ IDGS904ConBase/Models/Alumno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IDGS904ConBase.Models
+{
+    public class Alumno
+    {
+
+        public int Id { get; set;  }
+        [Required]
+        [StringLength(100)]
+        public string nombre { get; set;  }
+        [StringLength(150)]
+        public string Apaterno { get; set;  }
+        [StringLength(150)]
+        public string Amaterno { get; set;  }
+        public int Edad { get; set;  }
+        public string Correo { get; set;  }
+    }
+}
