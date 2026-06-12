5b369866dfbbb275f71236abdd132572b0614b60
Stefania Pro <stefaniapro5@gmail.com>
2026-06-11 23:28:14 -0600

    CRUD Maestros 11/06/2026

---
+++ IDGS904ConBase/Controllers/MaestrosController.cs
@@ -0,0 +1,115 @@
+﻿using IDGS904ConBase.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace IDGS904ConBase.Controllers
+{
+    public class MaestrosController : Controller
+    {
+        // GET: Maestros
+        public ActionResult Index()
+        {
+            using (var dbAlumno = new AlumnoContext())
+            {
+                return View(dbAlumno.Maestros.ToList());
+            }
+        }
+
+        // GET: Maestros/Details/5
+        public ActionResult Details(int id)
+        {
+            using (var dbAlumno = new AlumnoContext())
+            {
+                return View(dbAlumno.Maestros.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Maestros/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (var dbAlumno = new AlumnoContext())
+            {
+                return View(dbAlumno.Maestros.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Maestros/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Maestro maestro)
+        {
+            try
+            {
+                using (var dbAlumno = new AlumnoContext())
+                {
+                    dbAlumno.Entry(maestro).State = EntityState.Modified;
+                    dbAlumno.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Maestros/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Maestros/Create
+        [HttpPost]
+        public ActionResult Create(Maestro maestro)
+        {
+            try
+            {
+                using (var dbAlumno = new AlumnoContext())
+                {
+                    dbAlumno.Maestros.Add(maestro);
+                    dbAlumno.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Maestros/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (var dbAlumno = new AlumnoContext())
+            {
+                return View(dbAlumno.Maestros.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Maestros/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (var dbAlumno = new AlumnoContext())
+                {
+                    Maestro maestro = dbAlumno.Maestros.Where(x => x.Id == id).FirstOrDefault();
+
+                    dbAlumno.Maestros.Remove(maestro);
+                    dbAlumno.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ IDGS904ConBase/Models/Maestro.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IDGS904ConBase.Models
+{
+    public class Maestro
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [StringLength(100)]
+        public string Apaterno { get; set; }
+
+        [StringLength(100)]
+        public string Amaterno { get; set; }
+
+        public string Especialidad { get; set; }
+
+        public string Correo { get; set; }
+    }
+}
